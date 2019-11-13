025caca79c7e8b77e8cc8f942b0a778c232cbb17
carlsilber <carl.silber74@gmail.com>
2019-11-13 21:47:10 +0200

    Initial commit. Closes #1

---
+++ HELP.md
@@ -0,0 +1,4 @@
+# Simple Reddit Clone 
+
+Using Test Driven Development
+
---
+++ src/main/java/com/carlsilber/tddredditbackend/TddRedditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carlsilber.tddredditbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddRedditBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddRedditBackendApplication.class, args);
+    }
+
+}
