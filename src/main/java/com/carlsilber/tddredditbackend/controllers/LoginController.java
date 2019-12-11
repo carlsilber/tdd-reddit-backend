425bea63ff5ea14bb9e0780a0113f678fbf65c8e
carlsilber <carl.silber74@gmail.com>
2019-12-11 15:42:14 +0200

    handled login access for authenticated user. Closes #13

--- src/main/java/com/carlsilber/tddredditbackend/controllers/LoginController.java
+++ src/main/java/com/carlsilber/tddredditbackend/controllers/LoginController.java
@@ -1,14 +1,19 @@
 package com.carlsilber.tddredditbackend.controllers;
 
+import com.carlsilber.tddredditbackend.domain.User;
+import com.carlsilber.tddredditbackend.shared.CurrentUser;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class LoginController {
 
     @PostMapping("/api/1.0/login")
-    void handleLogin() {
-
+    Map<String, Object> handleLogin(@CurrentUser User loggedInUser) {
+        return Collections.singletonMap("id", loggedInUser.getId());
     }
 
 }
