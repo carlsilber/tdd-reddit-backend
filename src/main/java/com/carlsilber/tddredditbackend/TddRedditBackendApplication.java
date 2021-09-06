c4e688528d6fc6b0c828a3b33258e6e171eebb22
Carl Silber <carl.silber74@gmail.com>
2021-09-06 22:46:24 +0300

    Added command line runner to create dummy data (users) for the dev DB. closes #20

--- src/main/java/com/carlsilber/tddredditbackend/TddRedditBackendApplication.java
+++ src/main/java/com/carlsilber/tddredditbackend/TddRedditBackendApplication.java
@@ -1,7 +1,14 @@
 package com.carlsilber.tddredditbackend;
 
+import com.carlsilber.tddredditbackend.domain.User;
+import com.carlsilber.tddredditbackend.services.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class TddRedditBackendApplication {
@@ -10,4 +17,19 @@ public class TddRedditBackendApplication {
         SpringApplication.run(TddRedditBackendApplication.class, args);
     }
 
+    @Bean
+    @Profile("!Test")
+    CommandLineRunner run(UserService userService) {
+        return (args) -> {
+            IntStream.rangeClosed(1,15)
+                    .mapToObj(i -> {
+                        User user = new User();
+                        user.setUsername("user"+i);
+                        user.setDisplayName("display"+i);
+                        user.setPassword("P4ssword");
+                        return user;
+                    })
+                    .forEach(userService::save);
+        };
+    }
 }
