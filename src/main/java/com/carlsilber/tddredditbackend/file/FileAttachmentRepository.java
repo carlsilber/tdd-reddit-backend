235e6158fbd869cc047314295b16be5a74e5fd80
carlsilber <carl.silber74@gmail.com>
2021-09-30 16:11:33 +0300

    implemented deleting attachments that are not in use. closes #56

--- src/main/java/com/carlsilber/tddredditbackend/file/FileAttachmentRepository.java
+++ src/main/java/com/carlsilber/tddredditbackend/file/FileAttachmentRepository.java
@@ -2,6 +2,10 @@ package com.carlsilber.tddredditbackend.file;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long> {
 
+    List<FileAttachment> findByDateBeforeAndTopicIsNull(Date date);
 }
---
+++ src/test/java/com/carlsilber/tddredditbackend/FileAttachmentRepositoryTest.java
@@ -0,0 +1,100 @@
+package com.carlsilber.tddredditbackend;
+
+import com.carlsilber.tddredditbackend.domain.Topic;
+import com.carlsilber.tddredditbackend.file.FileAttachment;
+import com.carlsilber.tddredditbackend.file.FileAttachmentRepository;
+import com.carlsilber.tddredditbackend.services.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
+public class FileAttachmentRepositoryTest {
+
+    @Autowired
+    TestEntityManager testEntityManager;
+
+    @Autowired
+    FileAttachmentRepository fileAttachmentRepository;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void findByDateBeforeAndTopicIsNull_whenAttachmentsDateOlderThanOneHour_returnsAll() {
+        testEntityManager.persist(getOneHourOldFileAttachment());
+        testEntityManager.persist(getOneHourOldFileAttachment());
+        testEntityManager.persist(getOneHourOldFileAttachment());
+        Date oneHourAgo = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
+        List<FileAttachment> attachments = fileAttachmentRepository.findByDateBeforeAndTopicIsNull(oneHourAgo);
+        assertThat(attachments.size()).isEqualTo(3);
+    }
+
+    @Test
+    public void findByDateBeforeAndTopicIsNull_whenAttachmentsDateOlderThanOneHorButHaveTopic_returnsNone() {
+        Topic topic1 = testEntityManager.persist(TestUtil.createValidTopic());
+        Topic topic2 = testEntityManager.persist(TestUtil.createValidTopic());
+        Topic topic3 = testEntityManager.persist(TestUtil.createValidTopic());
+
+        testEntityManager.persist(getOldFileAttachmentWithTopic(topic1));
+        testEntityManager.persist(getOldFileAttachmentWithTopic(topic2));
+        testEntityManager.persist(getOldFileAttachmentWithTopic(topic3));
+        Date oneHourAgo = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
+        List<FileAttachment> attachments = fileAttachmentRepository.findByDateBeforeAndTopicIsNull(oneHourAgo);
+        assertThat(attachments.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void findByDateBeforeAndTopicIsNull_whenAttachmentsDateWithinOneHour_returnsNone() {
+        testEntityManager.persist(getFileAttachmentWithinOneHour());
+        testEntityManager.persist(getFileAttachmentWithinOneHour());
+        testEntityManager.persist(getFileAttachmentWithinOneHour());
+        Date oneHourAgo = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
+        List<FileAttachment> attachments = fileAttachmentRepository.findByDateBeforeAndTopicIsNull(oneHourAgo);
+        assertThat(attachments.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void findByDateBeforeAndTopicIsNull_whenSomeAttachmentsOldSomeNewAndSomeWithTopic_returnsAttachmentsWithOlderAndNoTopicAssigned() {
+        Topic topic1 = testEntityManager.persist(TestUtil.createValidTopic());
+        testEntityManager.persist(getOldFileAttachmentWithTopic(topic1));
+        testEntityManager.persist(getOneHourOldFileAttachment());
+        testEntityManager.persist(getFileAttachmentWithinOneHour());
+        Date oneHourAgo = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
+        List<FileAttachment> attachments = fileAttachmentRepository.findByDateBeforeAndTopicIsNull(oneHourAgo);
+        assertThat(attachments.size()).isEqualTo(1);
+    }
+
+    private FileAttachment getOneHourOldFileAttachment() {
+        Date date = new Date(System.currentTimeMillis() - (60 * 60 * 1000) - 1);
+        FileAttachment fileAttachment = new FileAttachment();
+        fileAttachment.setDate(date);
+        return fileAttachment;
+    }
+
+    private FileAttachment getFileAttachmentWithinOneHour() {
+        Date date = new Date(System.currentTimeMillis() - (60 * 1000));
+        FileAttachment fileAttachment = new FileAttachment();
+        fileAttachment.setDate(date);
+        return fileAttachment;
+    }
+
+    private FileAttachment getOldFileAttachmentWithTopic(Topic topic) {
+        FileAttachment fileAttachment = getOneHourOldFileAttachment();
+        fileAttachment.setTopic(topic);
+        return fileAttachment;
+    }
+
+}
